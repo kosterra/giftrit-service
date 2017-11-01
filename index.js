b8b4246ff6ef817c9c8aa4877a2c769df745709a
Ralph Koster <ralph.koster@clavisit.com>
2017-11-01 17:20:47 +0100

    Create REST service to read Hello World

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+// Allow Cross Origin
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
