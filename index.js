cde1f8a4c15babd02468f3a3a3e45347ee483258
Ralph Koster <ralph.koster@clavisit.com>
2017-12-10 19:36:18 +0100

    Created REST api with swagger

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 3000;
+var serverPort = process.env.PORT || 3000;
 
 // swaggerRouter configuration
 var options = {
