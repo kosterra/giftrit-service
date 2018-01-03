8bbcbc83bc7e3a03aa1ed9b6b28e078d353c3630
Ralph Koster <ralph.koster@clavisit.com>
2018-01-03 10:17:56 +0100

    playing around with sendgrid

--- routes/contactform.js
+++ routes/contactform.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var nodemailer = require('nodemailer');
-var mg = require('nodemailer-mailgun-transport');
+const sgMail = require('@sendgrid/mail');
 
-var API_KEY = process.env.MAILGUN_API_KEY;
-var MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN;
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 var auth = {
     auth: {
@@ -48,40 +46,18 @@ var auth = {
 router.post('/api/contact', function (req, res) {
     var from = req.body.from;
     var text = req.body.text;
-    var isError = false;
 
     console.log('\nCONTACT FORM DATA: '+ from + ': ' + text + '\n');
 
-    // create transporter object capable of sending email using the default SMTP transport
-    var transporter = nodemailer.createTransport(mg(auth));
-
-    // setup e-mail data with unicode symbols
-    var mailOptions = {
-        from: from, // sender address
-        to: 'ralph.koster@students.ffhs.ch', // list of receivers
-        subject: 'Contact-Message from Giftrit', // Subject line
+    const msg = {
+        to: 'ralph.koster@students.ffhs.ch',
+        from: from,
+        subject: 'Contact-Message from Giftrit',
         text: text,
-        err: isError
-
+        html: '<strong>text</strong>'
     };
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log('\nERROR: ' + error + '\n');
-            res.status(500)
-                .json({
-                    status: 'error',
-                    message: 'Failed to send email'
-                });
-        } else {
-            console.log('\nRESPONSE SENT: ' + info.response + '\n');
-            res.status(200)
-                .json({
-                    status: 'success',
-                    message: 'Email sent successfully'
-                });
-        }
-    });
+
+    res.status(sgMail.send(msg));
 });
 
 module.exports = router;
