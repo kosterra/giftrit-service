613d5a1d00ea75784d1d1713e4fa83b6a53d7dbc
Ralph Koster <ralph.koster@clavisit.com>
2018-01-03 21:14:12 +0100

    playing around with sendgrid

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ var swaggerSpec = swaggerJSDoc(options);
 
 // Allow Cross Origin
 app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
     next();
 });
 
