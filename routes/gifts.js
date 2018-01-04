e3d1342e7127bf44776d9443cfe6721513b6bd71
Ralph Koster <ralph.koster@clavisit.com>
2018-01-04 10:17:22 +0100

    trying to check access token from auth0

--- routes/gifts.js
+++ routes/gifts.js
@@ -3,23 +3,7 @@ const router = express.Router();
 
 const db = require('../queries/gifts');
 
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-
-const checkJwt = jwt({
-    // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://innt.eu.auth0.com/.well-known/jwks.json'
-    }),
-
-    // Validate the audience and the issuer.
-    audience: 'https://innt.eu.auth0.com/userinfo',
-    issuer: 'https://innt.eu.auth0.com',
-    algorithms: ['RS256']
-});
+const jwt = require('../helpers/jwt');
 
 /**
  * @swagger
@@ -104,7 +88,7 @@ router.get('/api/gifts/:id', db.getSingleGift);
  *       200:
  *         description: Successfully created
  */
-router.post('/api/gifts', checkJwt, db.createGift);
+router.post('/api/gifts', jwt.checkJwt, db.createGift);
 
 /**
  * @swagger
