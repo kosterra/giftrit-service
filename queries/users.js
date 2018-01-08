dd4686f3ef5911103e350389598297b20fc898f5
rudzitis <rudzitis@hispeed.ch>
2018-01-08 23:37:37 +0100

    Ergänzung gifts bei Abfrage user

--- queries/users.js
+++ queries/users.js
@@ -26,7 +26,34 @@ function getAllUsers(req, res, next) {
 
 function getSingleUser(req, res, next) {
     var userId = parseInt(req.params.id);
-    db.one('SELECT * FROM users WHERE id = $1', userId)
+	
+	let data = [];
+	
+    db.task(t => {
+        return t.one('SELECT * FROM users WHERE id = $1', userId)
+            .then(user => {                
+                    data = user;
+                    return t.any(
+                        'SELECT * FROM gifts WHERE userId = $1', user.id);				
+				}                
+            });
+	})
+	.then(gifts => {
+		data.gifts = gifts;
+		res.status(200)
+			.json({
+				status: 'success',
+				data: data,
+				message: 'Retrieved ONE user'
+			});
+	})
+	.catch(error => {
+		return next(error);
+	});
+	
+	
+	
+    /*db.one('SELECT * FROM users WHERE id = $1', userId)
         .then(function (data) {
             res.status(200)
                 .json({
@@ -37,7 +64,7 @@ function getSingleUser(req, res, next) {
         })
         .catch(function (err) {
             return next(err);
-        });
+        });*/
 }
 
 function createUser(req, res, next) {
