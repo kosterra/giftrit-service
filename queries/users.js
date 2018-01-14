3a238e359afab97e49e76c293b02cdb0d376febe
Ralph Koster <ralph.koster@clavisit.com>
2018-01-14 21:15:44 +0100

    update user

--- queries/users.js
+++ queries/users.js
@@ -87,8 +87,8 @@ function createUser(req, res, next) {
 }
 
 function updateUser(req, res, next) {
-    db.none('UPDATE users SET firstname=$1, lastname=$2, phone=$3, email=$4, karma=$5, description=$6, imageurl=$7 WHERE id=$8',
-        [req.body.firstname, req.body.lastname, req.body.phone, req.body.email, req.body.karma, req.body.description, req.body.imageUrl, req.params.id])
+    db.none('UPDATE users SET firstname=$1, lastname=$2, username=$3, phone=$4, email=$5, karma=$6, description=$7, imageurl=$8 WHERE id=$9',
+        [req.body.firstname, req.body.lastname, req.body.username, req.body.phone, req.body.email, req.body.karma, req.body.description, req.body.imageUrl, req.params.id])
         .then(function () {
             res.status(200)
                 .json({
